4fcf229dbc5a339607660b9d8a78d5690275ef50
Rebeca <becca9399@gmail.com>
2022-11-22 16:32:17 -0300

    Primeiro desafio em javaScript DIO

---
+++ JavaScript/primeiro-desafio-js/main.js
@@ -0,0 +1,17 @@
+/*
+
+Terá 3 variaveis, sendo elas:
+- Preço do combustível;
+- Gasto médio de combustivel do carro por KM;
+- Distancia em KM da viagem;
+
+*/
+
+const precoCombustivel = 5.79;
+const KmPorLitros = 10;
+const distanciaEmKm = 100;
+
+const litrosConsumidos = distanciaEmKm / KmPorLitros;
+const valorGasto = litrosConsumidos * precoCombustivel;
+
+console.log(valorGasto);
